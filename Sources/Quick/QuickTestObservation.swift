50cb1a90c59a93b46dc7cc1c4264bc755e075444
Tíarnán McGrath <tmcgrath@squarespace.com>
2022-10-03 15:35:56 +0100

    Using updated subclass detection to remove crash

--- Sources/Quick/QuickTestObservation.swift
+++ Sources/Quick/QuickTestObservation.swift
@@ -13,63 +13,25 @@ import XCTest
     @objc(sharedInstance)
     static let shared = QuickTestObservation()
 
+    private var didBuildAllExamples = false
+
     // Quick hooks into this event to compile example groups for each QuickSpec subclasses.
     //
     // If an exception occurs when compiling examples, report it to the user. Chances are they
     // included an expectation outside of a "it", "describe", or "context" block.
     func testBundleWillStart(_ testBundle: Bundle) {
-//        QuickSpec.enumerateSubclasses { specClass in
-//            // This relies on `_QuickSpecInternal`.
-//            (specClass as AnyClass).buildExamplesIfNeeded()
-//        }
+        buildAllExamplesIfNeeded()
     }
-}
 
-// swiftlint:disable:next todo
-// TODO: Unify this with QuickConfiguration's equivalent
-extension QuickSpec {
-    internal static func enumerateSubclasses(
-        subclasses: [QuickSpec.Type]? = nil,
-        _ block: (QuickSpec.Type) -> Void
-    ) {
-        let subjects: [QuickSpec.Type]
-        if let subclasses = subclasses {
-            subjects = subclasses
-        } else {
-            let classesCount = objc_getClassList(nil, 0)
+    @objc func buildAllExamplesIfNeeded() {
+        guard !didBuildAllExamples else { return }
+        didBuildAllExamples = true
 
-            guard classesCount > 0 else {
-                return
+        allSubclasses(ofType: QuickSpec.self)
+            .forEach { (specClass: QuickSpec.Type) in
+                // This relies on `_QuickSpecInternal`.
+                (specClass as AnyClass).buildExamplesIfNeeded()
             }
-
-            let classes = UnsafeMutablePointer<AnyClass?>.allocate(capacity: Int(classesCount))
-            defer { free(classes) }
-
-            objc_getClassList(AutoreleasingUnsafeMutablePointer(classes), classesCount)
-
-            var specSubclasses: [QuickSpec.Type] = []
-            for index in 0..<classesCount {
-                guard
-                    let subclass = classes[Int(index)],
-                    let superclass = class_getSuperclass(subclass),
-                    superclass is QuickSpec.Type
-                    else { continue }
-
-                // swiftlint:disable:next force_cast
-                specSubclasses.append(subclass as! QuickSpec.Type)
-            }
-
-            subjects = specSubclasses
-        }
-
-        subjects.forEach(block)
-    }
-    
-    @objc internal static func buildAllExamplesIfNeeded() {
-        QuickSpec.enumerateSubclasses { specClass in
-            // This relies on `_QuickSpecInternal`.
-            (specClass as AnyClass).buildExamplesIfNeeded()
-        }
     }
 }
 
